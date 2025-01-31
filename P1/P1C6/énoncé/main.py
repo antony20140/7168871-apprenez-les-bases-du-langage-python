97507380ae1e023ccba94488e563029dab9fd160
antony20140 <antoasie@hotmail.fr>
2025-01-31 10:53:47 +0100

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+fruits = ["pomme", "banane", "orange"]
+fruits.append("kiwi")
+fruits.remove("orange")
+fruits[1] = "ananas"
+print("La liste fruits contient", len(fruits), "éléments.")
+fruits.sort()
+fruits
