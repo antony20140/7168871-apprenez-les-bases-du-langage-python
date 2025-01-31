c4ffe72b02732dba891ea59a5508880053df593e
antony20140 <antoasie@hotmail.fr>
2025-01-31 09:48:05 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,12 +1,4 @@
-nom = ("priami")
+nom = "priami
 age = 46
 taille = 1.85
-etudiant = False
-print(f"Nom: {nom}")
-print(f"Age: {age}")
-print(f"Taille: {taille}")
-print(f"est étudiant: {est_etudiant}")
-print(f"Type nom: {type(nom)}")
-print(f"Type age: {type(age)}")
-print(f"Type taille: {type(taille)}")
-print(f"Type est étudiant: {type(est_etudiant)}")
+est_étudiant = True
