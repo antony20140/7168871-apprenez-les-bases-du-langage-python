5953d288193a4dbc1599d7b359dbfc4f682de698
antony20140 <antoasie@hotmail.fr>
2025-01-31 09:36:31 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -5,7 +5,7 @@ etudiant = False
 print(f"Nom: {nom}")
 print(f"Age: {age}")
 print(f"Taille: {taille}")
-print(f"Est étudiant: {est_etudiant}")
+print(f"est étudiant: {est_etudiant}")
 print(f"Type nom: {type(nom)}")
 print(f"Type age: {type(age)}")
 print(f"Type taille: {type(taille)}")
