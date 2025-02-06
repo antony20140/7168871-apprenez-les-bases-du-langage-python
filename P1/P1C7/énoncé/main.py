a5908c51cfe66b49d5386d0d9baef5e4bed69bfd
antony20140 <antoasie@hotmail.fr>
2025-02-06 13:56:09 +0100

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,6 +1,5 @@
 fruits={"pomme":"rouge", "banane":"jaune", "orange":"orange"}
 fruits["kiwi"] = "vert"
-fruits["banane"]'jaune'
 fruits["couleur_banane"] = "jaune"
 del fruits["orange"]
  print(fruits)
